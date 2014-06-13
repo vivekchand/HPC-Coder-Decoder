71df6d19bdc5964831ce6a6913782a87e8e94efe
Vivek Chand <vivek@aplopio.com>
2014-06-13 22:59:09 +0530

    proper indentation

--- MyOnlineCoder.cpp
+++ MyOnlineCoder.cpp
@@ -12,50 +12,50 @@
 
 class MyOnlineCoder
 {
-	public:
-		MyOnlineCoder(){ prevNum=0; iter_count=0; total_bits=0; bitPtr=0; bytePtr=0; 
-									   memset(buf,0,BUF_SZ);
-									 }
+    public:
+        MyOnlineCoder(){ prevNum=0; iter_count=0; total_bits=0; bitPtr=0; bytePtr=0; 
+                                       memset(buf,0,BUF_SZ);
+                                     }
 
-		// read next timestamp from input file and return as char*
-		char* getNextLine(FILE* fp);
-	
-		// convert timestamp into internal representaion and write to file
-		void codeNextTimestamp(char* tstamp_,FILE* output_file_);
+        // read next timestamp from input file and return as char*
+        char* getNextLine(FILE* fp);
 
-    		// get binary length of the given number
-    		int get_bin_length(long num);
+        // convert timestamp into internal representaion and write to file
+        void codeNextTimestamp(char* tstamp_,FILE* output_file_);
 
-		// get avg bits
-		double get_avg_bits() {return (total_bits*1.0)/(iter_count*1.0);}
+            // get binary length of the given number
+            int get_bin_length(long num);
 
-		// get count
-		int get_count() {return iter_count;}
+        // get avg bits
+        double get_avg_bits() {return (total_bits*1.0)/(iter_count*1.0);}
 
-		// convert char* timestamp to long long
-		long long convert2ll(char *tstamp_);
+        // get count
+        int get_count() {return iter_count;}
 
-		// write to buffer
-		void write_to_buf(uint8_t data,bool incr,FILE* fp);
-	
-		// get current buffer
-		uint8_t get_curr_byte();
-	
-    		// set 1s for the bit length of l
-    		int set_1s_for(int l);
-		
-		// Write remaining bytes if any
-		void write_rem(FILE *fp);	
-	private:
-		long long prevNum;
-		int iter_count;	
-		long long total_bits;
+        // convert char* timestamp to long long
+        long long convert2ll(char *tstamp_);
+
+        // write to buffer
+        void write_to_buf(uint8_t data,bool incr,FILE* fp);
+
+        // get current buffer
+        uint8_t get_curr_byte();
+
+            // set 1s for the bit length of l
+            int set_1s_for(int l);
+
+        // Write remaining bytes if any
+        void write_rem(FILE *fp);   
+    private:
+        long long prevNum;
+        int iter_count; 
+        long long total_bits;
 
 
-		int bitPtr,bytePtr;
-		uint8_t buf[BUF_SZ];
-		
-		
+        int bitPtr,bytePtr;
+        uint8_t buf[BUF_SZ];
+
+
 };
 
 // generate a sequence of l 1's
@@ -68,7 +68,7 @@ int MyOnlineCoder::set_1s_for(int l){
 
 uint8_t MyOnlineCoder::get_curr_byte()
 {
-	return buf[bytePtr];
+    return buf[bytePtr];
 }
 
 void MyOnlineCoder::write_to_buf(uint8_t data,bool incr,FILE *fp)
@@ -101,18 +101,18 @@ long long MyOnlineCoder::convert2ll(char *tstamp_){
 char* MyOnlineCoder::getNextLine(FILE* fp)
 {
   char * line = NULL;
-	size_t len = 0;
+    size_t len = 0;
   ssize_t read;
 
-	if(getline(&line, &len, fp)==-1)
-		return NULL;
+    if(getline(&line, &len, fp)==-1)
+        return NULL;
 
-	return line;
+    return line;
 }
 
 void MyOnlineCoder::write_rem(FILE *fp)
 {
-		// Write remaining bytes if any!
+        // Write remaining bytes if any!
     fwrite(buf,bytePtr,1,fp);
 }
 
@@ -130,129 +130,129 @@ int MyOnlineCoder::get_bin_length(long num){
 // code the Next TimeStamp
 void MyOnlineCoder::codeNextTimestamp(char* tstamp_,FILE* output_file_)
 {
-	char buffer[240];
-	int no_of_bits;
-	long long num;
-	int l;
-	num = convert2ll(tstamp_);
+    char buffer[240];
+    int no_of_bits;
+    long long num;
+    int l;
+    num = convert2ll(tstamp_);
 
-	// If it's the first timestamp, then it does no coding, i.e codes 
-	// as 'num1.num2' (as it is!)
-  	// Would store prevNum1 = num1 & prevNum2 = num2
-	if(prevNum==0){  // The First Time Stamp
-		int bits;
-		prevNum = num;
-    		fwrite(&prevNum, sizeof(prevNum),1, output_file_);  
-		no_of_bits=get_bin_length(prevNum);
-		// Write Data as it is for First TimeStamp
-	}	
-	else if(num==prevNum){
-			int type = 0;
-			int len = 0;
-			unsigned int header = type<<7|len;
-			if(bitPtr==0)
-			{
-				write_to_buf(header,true,output_file_);
-			}
-			else
-			{
-	  	  		uint8_t head1,head2;
-				head1 = get_curr_byte();
-			 	head1 |= header>>(bitPtr);
-		    		head2 = (header & set_1s_for(bitPtr))<<(8-bitPtr);
-			  	write_to_buf(head1,true,output_file_);                
-			  	write_to_buf(head2,false,output_file_);  
-			}
-      			no_of_bits = 1*8;
-	}
-	else // num != prevNum
-	{								
-		bool done;					
-		int diff,data;
-		diff = num - prevNum;
-		data = diff;
-		prevNum = num;
-		int type = 1;
-		int len = get_bin_length(diff);
-		int header = type<<7|len;
-		int total_len = len+8;
-		if(bitPtr==0)
-		{
-			write_to_buf(header,true,output_file_);
-			done = false;
-	    		while(!done){
-		    		uint8_t byte;
-    				if(len<8){
+    // If it's the first timestamp, then it does no coding, i.e codes 
+    // as 'num1.num2' (as it is!)
+    // Would store prevNum1 = num1 & prevNum2 = num2
+    if(prevNum==0){  // The First Time Stamp
+        int bits;
+        prevNum = num;
+            fwrite(&prevNum, sizeof(prevNum),1, output_file_);  
+        no_of_bits=get_bin_length(prevNum);
+        // Write Data as it is for First TimeStamp
+    }   
+    else if(num==prevNum){
+            int type = 0;
+            int len = 0;
+            unsigned int header = type<<7|len;
+            if(bitPtr==0)
+            {
+                write_to_buf(header,true,output_file_);
+            }
+            else
+            {
+                uint8_t head1,head2;
+                head1 = get_curr_byte();
+                head1 |= header>>(bitPtr);
+                    head2 = (header & set_1s_for(bitPtr))<<(8-bitPtr);
+                write_to_buf(head1,true,output_file_);                
+                write_to_buf(head2,false,output_file_);  
+            }
+                no_of_bits = 1*8;
+    }
+    else // num != prevNum
+    {                               
+        bool done;                  
+        int diff,data;
+        diff = num - prevNum;
+        data = diff;
+        prevNum = num;
+        int type = 1;
+        int len = get_bin_length(diff);
+        int header = type<<7|len;
+        int total_len = len+8;
+        if(bitPtr==0)
+        {
+            write_to_buf(header,true,output_file_);
+            done = false;
+                while(!done){
+                    uint8_t byte;
+                    if(len<8){
 
-	       				byte = ((data & set_1s_for(len))<<(8-len));
-	       				write_to_buf(byte & 0xFF,false,output_file_);
-	       				bitPtr = len;
-	       				done = true;
-		    		}
-		    		else
-		    		{
-	       				byte = (data>>(len-8));
-		     			write_to_buf(byte,true,output_file_);
-	       				len = len-8;
-	      			}
-	 	  	}
-		}
-		else
-		{ 		// If bitPtr is not pointing to 0
-     				uint8_t head1,head2;
-				head1 = get_curr_byte();
-     				head1 |= header>>(bitPtr);
-     				head2 = (header & set_1s_for(bitPtr))<<(8-bitPtr);
+                        byte = ((data & set_1s_for(len))<<(8-len));
+                        write_to_buf(byte & 0xFF,false,output_file_);
+                        bitPtr = len;
+                        done = true;
+                    }
+                    else
+                    {
+                        byte = (data>>(len-8));
+                        write_to_buf(byte,true,output_file_);
+                        len = len-8;
+                    }
+            }
+        }
+        else
+        {       // If bitPtr is not pointing to 0
+                    uint8_t head1,head2;
+                head1 = get_curr_byte();
+                    head1 |= header>>(bitPtr);
+                    head2 = (header & set_1s_for(bitPtr))<<(8-bitPtr);
 
-     				write_to_buf(head1,true,output_file_); 
-     				write_to_buf(head2,false,output_file_);
+                    write_to_buf(head1,true,output_file_); 
+                    write_to_buf(head2,false,output_file_);
 
-				done = false;
-				while(!done){
-	     				if(len<8){
-  	      					uint8_t byte,dat;
-	        				if(len<=(8-bitPtr)){
-	          					byte = data & set_1s_for(len);
-	          					dat = get_curr_byte();
-	          					dat |= byte<<(8-bitPtr-len);
+                done = false;
+                while(!done){
+                        if(len<8){
+                            uint8_t byte,dat;
+                            if(len<=(8-bitPtr)){
+                                byte = data & set_1s_for(len);
+                                dat = get_curr_byte();
+                                dat |= byte<<(8-bitPtr-len);
 
-							if((bitPtr+len)%8==0)
-		          					write_to_buf(dat & 0xFF,true,output_file_);
-							else
-		          					write_to_buf(dat & 0xFF,false,output_file_);
+                            if((bitPtr+len)%8==0)
+                                    write_to_buf(dat & 0xFF,true,output_file_);
+                            else
+                                    write_to_buf(dat & 0xFF,false,output_file_);
 
-					  		bitPtr = (bitPtr+len)%8;
-		      				}
-	        				else
-	        				{	
-	          					byte = data & set_1s_for(len);
-	          					dat = get_curr_byte();
+                            bitPtr = (bitPtr+len)%8;
+                            }
+                            else
+                            {   
+                                byte = data & set_1s_for(len);
+                                dat = get_curr_byte();
 
-							int	shift=bitPtr;
+                            int shift=bitPtr;
 
-	          	
-	          					dat |= byte>>(shift);
-	          					write_to_buf(dat,true,output_file_);
-	          					write_to_buf(((byte & set_1s_for(shift))<<(8-shift)),false,output_file_);
-	        				}
-	        				done = true;
-  	    				}
-	    				else
-					{
-		        			uint8_t byte,dat;
-		        			byte = data>>(len-8);
-		        			dat = get_curr_byte();
-		        			dat |= byte>>(bitPtr);
-		        			write_to_buf(dat,true,output_file_);
-		        			write_to_buf((byte & set_1s_for(bitPtr))<<(8-bitPtr),false,output_file_);
-		        			len = len-8;
-					}
-				}
-		}
-		no_of_bits = total_len;
-	}
-	total_bits+=no_of_bits;
-	iter_count++;
+
+                                dat |= byte>>(shift);
+                                write_to_buf(dat,true,output_file_);
+                                write_to_buf(((byte & set_1s_for(shift))<<(8-shift)),false,output_file_);
+                            }
+                            done = true;
+                        }
+                        else
+                    {
+                            uint8_t byte,dat;
+                            byte = data>>(len-8);
+                            dat = get_curr_byte();
+                            dat |= byte>>(bitPtr);
+                            write_to_buf(dat,true,output_file_);
+                            write_to_buf((byte & set_1s_for(bitPtr))<<(8-bitPtr),false,output_file_);
+                            len = len-8;
+                    }
+                }
+        }
+        no_of_bits = total_len;
+    }
+    total_bits+=no_of_bits;
+    iter_count++;
 }
 
 int main(int argc, char **argv)
@@ -269,7 +269,7 @@ int main(int argc, char **argv)
     exit(-2);
   }
 
-  out_fp = fopen("coded_timestamps","wb");	
+  out_fp = fopen("coded_timestamps","wb");  
   if (out_fp == NULL) {
     printf("Failed to open %s for writing.\n","coded_timestamps");
     exit(-3);
@@ -281,46 +281,46 @@ int main(int argc, char **argv)
     exit(-4);
   }
 
-	MyOnlineCoder coder;
-	char *tstamp;
+    MyOnlineCoder coder;
+    char *tstamp;
 
-  	struct timeval startTime;
-  	struct timeval endTime;
-	double tS,tE,time;
-	double total_time=0;
-	double t[451210];	
-	long size=0;
-	while((tstamp=coder.getNextLine(fp))){
-		gettimeofday(&startTime, NULL);   // get the start time
-			coder.codeNextTimestamp(tstamp,out_fp);
-		gettimeofday(&endTime, NULL);  // get the end time
-    		tS = startTime.tv_sec*1000000 + (startTime.tv_usec);
-    		tE = endTime.tv_sec*1000000  + (endTime.tv_usec);
-		time = tE - tS;
-		t[size]=time;
-		size++;
-		total_time += time;
-		free(tstamp);
-	}
-	coder.write_rem(out_fp);
-	// Caluclation of Standard Deviation	
-	double avg_time,mean,sum_of_diff,variance,std_dev;
-	avg_time = mean = total_time/coder.get_count();
-	
-	for(int i=0;i<size;i++)
-		sum_of_diff += (t[i]-mean)*(t[i]-mean);
+    struct timeval startTime;
+    struct timeval endTime;
+    double tS,tE,time;
+    double total_time=0;
+    double t[451210];   
+    long size=0;
+    while((tstamp=coder.getNextLine(fp))){
+        gettimeofday(&startTime, NULL);   // get the start time
+            coder.codeNextTimestamp(tstamp,out_fp);
+        gettimeofday(&endTime, NULL);  // get the end time
+            tS = startTime.tv_sec*1000000 + (startTime.tv_usec);
+            tE = endTime.tv_sec*1000000  + (endTime.tv_usec);
+        time = tE - tS;
+        t[size]=time;
+        size++;
+        total_time += time;
+        free(tstamp);
+    }
+    coder.write_rem(out_fp);
+    // Caluclation of Standard Deviation    
+    double avg_time,mean,sum_of_diff,variance,std_dev;
+    avg_time = mean = total_time/coder.get_count();
+
+    for(int i=0;i<size;i++)
+        sum_of_diff += (t[i]-mean)*(t[i]-mean);
  
-	variance = sum_of_diff/(coder.get_count()-1);
-	std_dev = sqrt(variance);
+    variance = sum_of_diff/(coder.get_count()-1);
+    std_dev = sqrt(variance);
 
-	fprintf(stats," ---- COMPRESSION STATISTICS --- ");
-	fprintf(stats,"\nDegree of compression(Avg bits): %f",coder.get_avg_bits());
-	fprintf(stats,"\nAvg time to compress in usecs: %.0lf",avg_time);
-	fprintf(stats,"\nMean:  %.0lf",mean);
-	fprintf(stats,"\nStandard Deviation:  %.0lf\n",std_dev);
+    fprintf(stats," ---- COMPRESSION STATISTICS --- ");
+    fprintf(stats,"\nDegree of compression(Avg bits): %f",coder.get_avg_bits());
+    fprintf(stats,"\nAvg time to compress in usecs: %.0lf",avg_time);
+    fprintf(stats,"\nMean:  %.0lf",mean);
+    fprintf(stats,"\nStandard Deviation:  %.0lf\n",std_dev);
 
-	fclose(fp);
-	fclose(out_fp);
-	fclose(stats);
-	return 0;
+    fclose(fp);
+    fclose(out_fp);
+    fclose(stats);
+    return 0;
 }
--- MyOnlineDecoder.cpp
+++ MyOnlineDecoder.cpp
@@ -17,55 +17,55 @@
 
 class MyOnlineDecoder
 {
-	public:
+    public:
 
-		MyOnlineDecoder(){count=0; total_time=0; size=0; bitPtr=0; bytePtr=0; memset(buf,0,BUF_SZ); }
-		// parse input file and decode - this function should
-		// incrementally decode and write every new timestamp
-		// to output file
-		void parseAndDecode(FILE* input_file_,FILE* output_file_);
-		
-		// get count		
-		int get_count(){return count;}
-		
-		// get avg time
-		double get_avg_time(){return total_time/get_count();}
+        MyOnlineDecoder(){count=0; total_time=0; size=0; bitPtr=0; bytePtr=0; memset(buf,0,BUF_SZ); }
+        // parse input file and decode - this function should
+        // incrementally decode and write every new timestamp
+        // to output file
+        void parseAndDecode(FILE* input_file_,FILE* output_file_);
+        
+        // get count        
+        int get_count(){return count;}
+        
+        // get avg time
+        double get_avg_time(){return total_time/get_count();}
 
-		// get mean
-		double get_mean(){return total_time/get_count(); }
+        // get mean
+        double get_mean(){return total_time/get_count(); }
 
-		// get standard deviation
-		double get_std_dev();
+        // get standard deviation
+        double get_std_dev();
 
-		// get next number
-		long long getNextNum(FILE* fp);
+        // get next number
+        long long getNextNum(FILE* fp);
 
-		int getCodeType(FILE* fp);
+        int getCodeType(FILE* fp);
 
-		// convert long long to char*
-		char* convert2str(long long num);
+        // convert long long to char*
+        char* convert2str(long long num);
 
-		// Read from buf
-		void read_frm_buf(uint8_t *data,bool incr,FILE* fp);
+        // Read from buf
+        void read_frm_buf(uint8_t *data,bool incr,FILE* fp);
 
-    		// set 1s for the bit length of l
-    		int set_1s_for(int l);
+            // set 1s for the bit length of l
+            int set_1s_for(int l);
 
 
-	private:
-		int count;
-    		long long prevNum;
-		int num2_len;
-		struct timeval startTime;
-  		struct timeval endTime;
-  		double tS,tE,time;
-  		double total_time;
+    private:
+        int count;
+            long long prevNum;
+        int num2_len;
+        struct timeval startTime;
+        struct timeval endTime;
+        double tS,tE,time;
+        double total_time;
 
-		double t[SIZE];
-  		long size;
+        double t[SIZE];
+        long size;
 
-    		int bitPtr,bytePtr;
-    		uint8_t buf[BUF_SZ];
+            int bitPtr,bytePtr;
+            uint8_t buf[BUF_SZ];
 
 };
 
@@ -109,153 +109,153 @@ char* MyOnlineDecoder::convert2str(long long num){
 }
 
 double MyOnlineDecoder::get_std_dev(){
-  	// Caluclation of Standard Deviation  
-  	double avg_time,mean,sum_of_diff,variance,std_dev;
-  	avg_time = mean = total_time/get_count();
+    // Caluclation of Standard Deviation  
+    double avg_time,mean,sum_of_diff,variance,std_dev;
+    avg_time = mean = total_time/get_count();
 
-  	for(int i=0;i<size;i++)
-    		sum_of_diff += (t[i]-mean)*(t[i]-mean);
+    for(int i=0;i<size;i++)
+            sum_of_diff += (t[i]-mean)*(t[i]-mean);
 
-  	variance = sum_of_diff/(get_count()-1);
-  	std_dev = sqrt(variance);
+    variance = sum_of_diff/(get_count()-1);
+    std_dev = sqrt(variance);
 
- 	return std_dev;
+    return std_dev;
 }
 
 // get the next coded num from the binary file
 long long MyOnlineDecoder::getNextNum(FILE* fp){
-	long long x;
-	int ret;
-	ret = fread(&x, sizeof(x), 1, fp);
-	if(ret == 0)
-		return -1;
-	return x;
+    long long x;
+    int ret;
+    ret = fread(&x, sizeof(x), 1, fp);
+    if(ret == 0)
+        return -1;
+    return x;
 }
 
 int MyOnlineDecoder::getCodeType(FILE* fp){
-	uint8_t header=0;
-	if(bitPtr==0)
-		read_frm_buf(&header,true,fp);
-	else{
-    		uint8_t head1,head2;
-    		read_frm_buf(&head1,true,fp);
-    		read_frm_buf(&head2,false,fp);
-	  	header = head1<<bitPtr | head2>>(8-bitPtr);
-	}
+    uint8_t header=0;
+    if(bitPtr==0)
+        read_frm_buf(&header,true,fp);
+    else{
+            uint8_t head1,head2;
+            read_frm_buf(&head1,true,fp);
+            read_frm_buf(&head2,false,fp);
+        header = head1<<bitPtr | head2>>(8-bitPtr);
+    }
   return header;
 }
 
 // Parse & Decode the encoded file
 void MyOnlineDecoder::parseAndDecode(FILE* input_file_,FILE* output_file_){
-	fpos_t position;
-	int first=1;
-	long long num;
-	int n;
-	int l1,l2;
-	int num1,num2;
-	int x;
-  	while(1){ 
-		if(count>=SIZE)
-			break;
-		count++;
-		gettimeofday(&startTime, NULL);   // get the start time
-		char *stuffed;
-		// If it's the first coded timestamp, then it does no decoding, i.e decodes 
-		// as 'num1.num2' (as it is)
-		// Would store prevNum1 = num1 & prevNum2 = num2
-		if(first){ // The First Coded Time Stamp
-        		prevNum = getNextNum(input_file_);
-        		fprintf(output_file_,"%s",convert2str(prevNum));
-        		first = 0;
-			fread(buf,BUF_SZ,1,input_file_);
-		}  
-		else { 
-			x = getCodeType(input_file_);
-			int type,len;
-			type = x>>7;
-			len = x & 0x7F;
-			if(type==0){ 
-	        		fprintf(output_file_,"%s",convert2str(prevNum));
-			} 
-			else if(type==1){ 
-				int diff;
-				unsigned int data=0;
-				bool done;
-				if(bitPtr==0)
-				{ 
-					done = false;
-					while(!done){ 
-						uint8_t byte=0;
-					        if(len<8){ 
-							read_frm_buf(&byte,false,input_file_);
-					        	data |= byte>>(8-len);
-					        	bitPtr = len;
-					        	done = true;
-					       } 
-					       else
-		     			       { 		
-							read_frm_buf(&byte,true,input_file_);
-			 			      	data |= byte<<(len-8);
-	  	      					len = len-8;
-	    	  				}	 
-    					}// end while	
-				} 
-				else 
-				{	
-					data = 0; 
-					done = false;
-					while(!done){ 
-					        if(len<8)
-					        {
-					         	uint8_t byte,dat;
-					         	if(len<=(8-bitPtr)){
-								if((bitPtr+len)%8==0)
-					            			read_frm_buf(&dat,true,input_file_);
-								else
-					            			read_frm_buf(&dat,false,input_file_);
-			
-				            			byte = dat>>(8-bitPtr-len) & set_1s_for(len);
-				            			bitPtr = (bitPtr+len)%8;
-				            			data |= byte;
-				          		}
-				          		else
-				          		{
-				           	 		uint8_t byte1,byte2;
-				            			read_frm_buf(&byte1,true,input_file_);
-				            			read_frm_buf(&byte2,false,input_file_);
-		
-								int shift = (bitPtr);
-		
-				            			byte = (byte1 & set_1s_for(8-shift))<<(shift) | byte2>>(8-shift);
-				            			data |= byte;
-				          		}
-				          		done = true;
-	    			   		}
-						else
-						{ 
-					          	uint8_t byte1,byte2,byte;
-					          	read_frm_buf(&byte1,true,input_file_);
-					          	read_frm_buf(&byte2,false,input_file_);
-					          	byte = byte1<<(bitPtr) | byte2>>(8-bitPtr);
-					          	data |= byte<<(len-8);
-					          	len = len-8;
-						} 
-					} // end while 
-				} 
-				diff = data;
-				prevNum = prevNum + diff;
-        			fprintf(output_file_,"%s",convert2str(prevNum));
-		} 	
-  	} 
-  	fprintf(output_file_,"\n");
-  	gettimeofday(&endTime, NULL);  // get the end time
-	tS = startTime.tv_sec*1000000 + (startTime.tv_usec);
-	tE = endTime.tv_sec*1000000  + (endTime.tv_usec);
-	time = tE - tS;
-	t[size]=time;
-	size++;
-	total_time += time;
- 	} 
+    fpos_t position;
+    int first=1;
+    long long num;
+    int n;
+    int l1,l2;
+    int num1,num2;
+    int x;
+    while(1){ 
+        if(count>=SIZE)
+            break;
+        count++;
+        gettimeofday(&startTime, NULL);   // get the start time
+        char *stuffed;
+        // If it's the first coded timestamp, then it does no decoding, i.e decodes 
+        // as 'num1.num2' (as it is)
+        // Would store prevNum1 = num1 & prevNum2 = num2
+        if(first){ // The First Coded Time Stamp
+                prevNum = getNextNum(input_file_);
+                fprintf(output_file_,"%s",convert2str(prevNum));
+                first = 0;
+            fread(buf,BUF_SZ,1,input_file_);
+        }  
+        else { 
+            x = getCodeType(input_file_);
+            int type,len;
+            type = x>>7;
+            len = x & 0x7F;
+            if(type==0){ 
+                    fprintf(output_file_,"%s",convert2str(prevNum));
+            } 
+            else if(type==1){ 
+                int diff;
+                unsigned int data=0;
+                bool done;
+                if(bitPtr==0)
+                { 
+                    done = false;
+                    while(!done){ 
+                        uint8_t byte=0;
+                            if(len<8){ 
+                            read_frm_buf(&byte,false,input_file_);
+                                data |= byte>>(8-len);
+                                bitPtr = len;
+                                done = true;
+                           } 
+                           else
+                               {        
+                            read_frm_buf(&byte,true,input_file_);
+                                data |= byte<<(len-8);
+                                len = len-8;
+                            }    
+                        }// end while   
+                } 
+                else 
+                {   
+                    data = 0; 
+                    done = false;
+                    while(!done){ 
+                            if(len<8)
+                            {
+                                uint8_t byte,dat;
+                                if(len<=(8-bitPtr)){
+                                if((bitPtr+len)%8==0)
+                                            read_frm_buf(&dat,true,input_file_);
+                                else
+                                            read_frm_buf(&dat,false,input_file_);
+            
+                                        byte = dat>>(8-bitPtr-len) & set_1s_for(len);
+                                        bitPtr = (bitPtr+len)%8;
+                                        data |= byte;
+                                }
+                                else
+                                {
+                                    uint8_t byte1,byte2;
+                                        read_frm_buf(&byte1,true,input_file_);
+                                        read_frm_buf(&byte2,false,input_file_);
+        
+                                int shift = (bitPtr);
+        
+                                        byte = (byte1 & set_1s_for(8-shift))<<(shift) | byte2>>(8-shift);
+                                        data |= byte;
+                                }
+                                done = true;
+                            }
+                        else
+                        { 
+                                uint8_t byte1,byte2,byte;
+                                read_frm_buf(&byte1,true,input_file_);
+                                read_frm_buf(&byte2,false,input_file_);
+                                byte = byte1<<(bitPtr) | byte2>>(8-bitPtr);
+                                data |= byte<<(len-8);
+                                len = len-8;
+                        } 
+                    } // end while 
+                } 
+                diff = data;
+                prevNum = prevNum + diff;
+                    fprintf(output_file_,"%s",convert2str(prevNum));
+        }   
+    } 
+    fprintf(output_file_,"\n");
+    gettimeofday(&endTime, NULL);  // get the end time
+    tS = startTime.tv_sec*1000000 + (startTime.tv_usec);
+    tE = endTime.tv_sec*1000000  + (endTime.tv_usec);
+    time = tE - tS;
+    t[size]=time;
+    size++;
+    total_time += time;
+    } 
 }
 
 int main(int argc, char **argv)
@@ -286,16 +286,16 @@ int main(int argc, char **argv)
 
 
 
-	MyOnlineDecoder decoder;	
-	decoder.parseAndDecode(fp,out_fp);
+    MyOnlineDecoder decoder;    
+    decoder.parseAndDecode(fp,out_fp);
 
-  	fprintf(stats,"\n ---- DECOMPRESSION STATISTICS --- ");
-	fprintf(stats,"\nAvg time to decompress in usecs: %.0lf",decoder.get_avg_time());
-  	fprintf(stats,"\nMean:  %.0lf",decoder.get_mean());
-  	fprintf(stats,"\nStandard Deviation:  %.0lf\n",decoder.get_std_dev());
+    fprintf(stats,"\n ---- DECOMPRESSION STATISTICS --- ");
+    fprintf(stats,"\nAvg time to decompress in usecs: %.0lf",decoder.get_avg_time());
+    fprintf(stats,"\nMean:  %.0lf",decoder.get_mean());
+    fprintf(stats,"\nStandard Deviation:  %.0lf\n",decoder.get_std_dev());
 
-	fclose(fp);
-	fclose(out_fp);
-	fclose(stats);
-	return 0;	
+    fclose(fp);
+    fclose(out_fp);
+    fclose(stats);
+    return 0;   
 }
